ce7a844e5fb33436b963fbbdd0631330116a7bed
Krishna Vishwanath Kungiri <krishnakun@cybage.com>
2024-07-17 12:08:22 +0530

    Configured jest testing framework using official docs

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('DataService', () => {
   let service: DataService;
-
+  let httpMock: HttpClient;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        DataService,
+        { provide: HttpClient, useValue: { get: jest.fn() } },
+      ],
+    });
     service = TestBed.inject(DataService);
+    httpMock = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
